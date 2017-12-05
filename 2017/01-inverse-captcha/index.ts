1882b67e445acfeed4769db66425c6ece32ea24e
Dustin Schau <dustinschau@gmail.com>
2017-12-04 20:30:00 -0600

    refactor: improve day 1 algo

--- 2017/01-inverse-captcha/index.ts
+++ 2017/01-inverse-captcha/index.ts
@@ -1,17 +1,12 @@
 type GetNextIndex = (index: number, range: number[]) => number;
 
-// TODO: improve this algorithm!
 export function inverseCaptcha(input: string, getNext: GetNextIndex): number {
   const range: number[] = input.split('').map(value => parseInt(value, 10));
-  const count = range.reduce((matches, num, index) => {
-    if (num === range[getNext(index, range)]) {
-      matches[num] = (matches[num] || 0) + 1;
+  return range.reduce((sum, num, index) => {
+    const next = getNext(index, range);
+    if (num === range[next]) {
+      sum += num;
     }
-    return matches;
-  }, {});
-
-  return Object.keys(count).reduce((sum, key) => {
-    const value = parseInt(key, 10);
-    return sum + value * count[key];
+    return sum;
   }, 0);
 }
