f7a324231da8a2bda0039d5afadaf021ad26de2e
Dustin Schau <dustinschau@gmail.com>
2017-12-05 16:54:53 -0600

    chore: remove unneeded temp variables

--- 2017/2017-a-maze-of-twisty-trampolines-all-alike/index.ts
+++ 2017/2017-a-maze-of-twisty-trampolines-all-alike/index.ts
@@ -4,9 +4,8 @@ export function getMazeSteps(input: number[], modifier?: number) {
   const clone = input.slice(0);
   while (index >= 0 && index < input.length) {
     const jump = clone[index];
-    const temp = index;
+    clone[index] = modifier && jump >= modifier ? jump - 1 : jump + 1;
     index += jump;
-    clone[temp] = modifier && jump >= modifier ? jump - 1 : jump + 1;
     steps += 1;
   }
   return steps;
