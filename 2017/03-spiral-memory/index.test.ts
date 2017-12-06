988d97baf96b347ffd4c448f24c99c2afd0521ad
Dustin Schau <dustinschau@gmail.com>
2017-12-05 20:16:31 -0600

    chore: add a way to programatically generate input

--- 2017/03-spiral-memory/index.test.ts
+++ 2017/03-spiral-memory/index.test.ts
@@ -1,11 +1,13 @@
 import { spiralMemory } from './';
 
+const { input: INPUT } = require('./input.json');
+
 [
   [1, 0],
   [12, 3],
   [23, 2],
   [1024, 31],
-  [277678, 475]
+  [INPUT, 475]
 ].forEach(([input, output]) => {
   test(`it handles ${input}`, () => {
     expect(spiralMemory(input)).toBe(output);
---
+++ generate-input.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+
+const base = path.join(__dirname, '2017');
+const getUrl = day => `http://adventofcode.com/2017/day/${day}/input`;
+
+const folders = fs.readdirSync(base)
+  .filter(fileOrFolder => fs.statSync(path.join(base, fileOrFolder)).isDirectory())
+  .filter(folder => !fs.existsSync(path.join(base, folder, 'input.json')));
+
+const writeFile = (...args) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(...args.concat((err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    }));
+  })
+}
+
+(async () => {
+  await Promise.all(
+    folders
+      .map(async folder => {
+        const day = parseFloat(folder);
+        const input = await axios.get(getUrl(day), {
+          headers: {
+            ContentType: 'text/plain',
+            Cookie: `session=${process.env.SESSION_COOKIE};`
+          },
+          transformResponse: [data => data]
+        })
+          .then(response => {
+            return response;
+          })
+          .then(response => {
+            const data = response.data.trim();
+            const transformed = data.split(/\s+/g).map(part => {
+              const num = parseFloat(part);
+              if (num === NaN || num === Infinity) {
+                return part;
+              }
+              return num;
+            });
+            return { input: transformed.length === 1 ? transformed.pop() : transformed };
+          });
+
+        const filePath = path.join(base, folder, 'input.json');
+        return await writeFile(filePath, JSON.stringify(input, null, 2), 'utf8');
+      })
+  )
+    .then(files => {
+      console.log(`Successfully wrote ${files.length} files`);
+    });
+})();
